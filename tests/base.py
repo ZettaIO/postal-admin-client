fc24ead8e91a76e543ade42ddcd3bbb8c4a5f796
Einar Forselv <eforselv@gmail.com>
2019-06-19 00:03:06 +0200

    Base testcase

---
+++ tests/base.py
@@ -0,0 +1,20 @@
+from unittest import TestCase, mock
+
+import postal_admin_client
+
+
+class PostalAdminTestcase(TestCase):
+    client = None
+    orgs = [
+        {'name': 'Anansi Technologies', 'shortname': 'at'},
+        {'name': 'Council of Venice', 'shortname': 'cov'},
+        {'name': 'Orochi Group', 'shortname': 'og'},
+    ]
+
+    @classmethod
+    def setUpClass(self):
+        self.client = postal_admin_client.Client(
+            'https://somewhere',
+            email='me@somewhere',
+            password='mypass',
+        )
