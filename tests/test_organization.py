2dae5b0acecb47569a3725f5a28ab56fd4405f00
Einar Forselv <eforselv@gmail.com>
2019-06-19 00:24:30 +0200

    More consistent fixture names

--- tests/test_organization.py
+++ tests/test_organization.py
@@ -9,6 +9,6 @@ class OrganizationTests(PostalAdminTestcase):
 
     def test_list(self):
         """List organization"""
-        with mock.patch(REQUEST_FUNC, new=fixture_response('list_org.html')):
+        with mock.patch(REQUEST_FUNC, new=fixture_response('org_list.html')):
             orgs = self.client.list_organizations()
             self.assertEqual(orgs, self.orgs)
