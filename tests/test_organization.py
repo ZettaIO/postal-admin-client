c3605ab04be3f21e097e0791ebfd4d1b8781219b
Einar Forselv <eforselv@gmail.com>
2019-06-19 00:04:38 +0200

    Basic organization list test

---
+++ tests/test_organization.py
@@ -0,0 +1,14 @@
+from unittest import TestCase, mock
+from base import PostalAdminTestcase
+from utils import fixture_response
+
+REQUEST_FUNC = 'requests.sessions.Session.request'
+
+
+class OrganizationTests(PostalAdminTestcase):
+
+    def test_list(self):
+        """List organization"""
+        with mock.patch(REQUEST_FUNC, new=fixture_response('list_org.html')):
+            orgs = self.client.list_organizations()
+            self.assertEqual(orgs, self.orgs)
