34af7ec5659ce0c75600a679ed7f246d62e36155
Einar Forselv <eforselv@gmail.com>
2019-06-19 03:05:01 +0200

    Improve test utilities with default fixtures

--- tests/utils.py
+++ tests/utils.py
@@ -1,18 +1,58 @@
+import json as js
 from pathlib import Path
+from urllib.parse import urlparse
+
 from requests.exceptions import HTTPError
 
 FIXTURE_DIR = Path(__file__).parent / 'fixtures'
 
+# path: file
+FIXTURES = {
+    'GET:': {'file': 'org_list.html'},
+    'GET:/login': {'file': 'login.html'},
+    'POST:/login': {'file': 'org_list.html'},
+    'GET:/organizations/new': {'file': 'org_new.html'}
+}
 
-def fixture_response(fixture, status_code=200):
+def load_fixture(name: str):
+    with open(FIXTURE_DIR / name, mode='rb') as fd:
+        return fd.read()
+
+
+def get_fixture(path):
+    entry = FIXTURES.get(path)
+    if entry is not None:
+        if 'file' in entry:
+            return load_fixture(entry['file'])
+        if 'content' in entry:
+            return entry['content']
+        if 'json' in entry:
+            return js.dumps(entry['json'])
+
+    raise ValueError("No fixture found for path {}".format(path))
+
+
+def fixture_response(override_method=None, override_path=None, fixture=None, json=None, status_code=200):
     """Creates a fake request method returning an http response with a specific response"""
-    with open(FIXTURE_DIR / fixture, mode='rb') as fd:
-        content = fd.read()
+    if fixture:
+        override_content = load_fixture(fixture)
 
-    def request(*args, **kwargs):
-        # content = open()
+    if json:
+        override_content = js.dumps(json).encode()
+
+    def request(sess, method, url, *args, **kwargs):
+        path = urlparse(url).path
+        fixture_key = "{}:{}".format(method, path)
+        print("session", method, path)
+
+        if path == override_path and method == override_method:
+            content = override_content
+        else:
+            content = get_fixture(fixture_key)
+
         return FakeHttpResponse(
             status_code=status_code,
+            url=url,
             content=content,
         )
     return request
@@ -21,17 +61,25 @@ def fixture_response(fixture, status_code=200):
 class FakeHttpResponse:
     encoding = 'utf-8'
 
-    def __init__(self, content=None, status_code=200):
+    def __init__(self, url=None, content=None, status_code=200):
         self._content = content
         self._status_code = status_code
+        self._url = url
 
     @property
     def status_code(self):
         return self._status_code
 
     @property
+    def url(self):
+        return self._url
+
+    @property
     def content(self) -> bytes:
         return self._content
+
+    def json(self):
+        return js.loads(self._content)
 
     def raise_for_status(self):
         if self._status_code > 400:
