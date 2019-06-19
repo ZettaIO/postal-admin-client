484faf2de81ea9aa2080afd0b1849c10ed13c2a0
Einar Forselv <eforselv@gmail.com>
2019-06-19 05:36:46 +0200

    Implment Create/invite user

--- postal_admin_client/client.py
+++ postal_admin_client/client.py
@@ -95,3 +95,37 @@ class Client:
         # Wrong pw response: {"alert":"The password you entered was invalid. Please check and try again."}
         if 'alert' in data:
             raise ValueError("Incorrect password entered when deleting organizaton")
+
+    def user_list(self, shortname: str):
+        pass
+
+    def create_user(self, shortname: str, email: str, admin=False):
+        """
+        Create/invite a user in an organization.
+
+        An email will be sent to the user with an invite.
+        The first person invited to an organization will automatically be the owner.
+        If the user already exist with the supplied email in postal
+        they will get a simplified mail accepting an organization invite.
+        If the user do not already have a user in postal a new user form is presented.
+
+        Args:
+            shortname (str): The organization
+            email (str): New user email
+
+        Keyword Args:
+            admin (bool): Should the user be an administrator?
+        """
+        self._http.get('org/{}/users/new'.format(shortname))
+        self._http.post(
+            'org/{}/users'.format(shortname),
+            data={
+                'utf8': True,
+                'invite': '',
+                'organization_user[email_address]': email,
+                'organization_user[admin]': admin,
+                'commit': 'Add+User',
+            },
+        )
+        # 422 when email is invalid
+        # {"redirect_to":"https://postal.test/org/nada/users"}
