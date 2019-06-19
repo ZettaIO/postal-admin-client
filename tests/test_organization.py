60178deb175e81017e86c976fe55560176a4666e
Einar Forselv <eforselv@gmail.com>
2019-06-19 03:06:12 +0200

    Test creating a new org

--- tests/test_organization.py
+++ tests/test_organization.py
@@ -9,6 +9,13 @@ class OrganizationTests(PostalAdminTestcase):
 
     def test_list(self):
         """List organization"""
-        with mock.patch(REQUEST_FUNC, new=fixture_response('org_list.html')):
+        with mock.patch(REQUEST_FUNC, new=fixture_response()):
             orgs = self.client.list_organizations()
             self.assertEqual(orgs, self.orgs)
+
+    def test_create(self):
+        """Create an organization"""
+        json = {"redirect_to": "/org/il"}
+        with mock.patch(REQUEST_FUNC, new=fixture_response('POST', '/organizations', json=json)):
+            org = self.client.create_organization('Illuminati')
+            self.assertEqual(org, {'name': 'Illuminati', 'shortname': 'il'})
