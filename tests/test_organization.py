48a88a173221c529102d71f2a0f63afe9e4ae82b
Einar Forselv <eforselv@gmail.com>
2019-06-19 04:56:22 +0200

    Test org delete

--- tests/test_organization.py
+++ tests/test_organization.py
@@ -20,8 +20,28 @@ class OrganizationTests(PostalAdminTestcase):
             self.assertEqual(org, {'name': 'Illuminati', 'shortname': 'il'})
 
     def test_create_existing(self):
-        """Esure 422 error is picked up when shortname is taken"""
+        """Ensure 422 error is picked up when shortname is taken"""
         json = {"error": "Shortname already taken"}
         with fixtures('POST', '/organizations', json=json, status_code=422):
             with self.assertRaises(HTTPError):
                 org = self.client.create_organization('Illuminati', shortname='il')
+
+    def test_delete(self):
+        """Test a simple delete"""
+        json = {"redirect_to":"/?nrd=1"}
+        with fixtures('POST', '/org/nada/delete', json=json):
+            self.client.delete_organization('nada')
+
+    def test_delete_wrong_pw(self):
+        """Response when password is wrong"""
+        json = {"alert":"The password you entered was invalid. Please check and try again."}
+        with fixtures('POST', '/org/nada/delete', json=json):
+            with self.assertRaises(ValueError):
+                self.client.delete_organization('nada')
+
+    def test_delete_nonexistent(self):
+        """Ensure trying to delete a non-existent forwards 404"""
+        json = {"status":404,"error":"Not Found"}
+        with fixtures('POST', '/org/nada/delete', json=json, status_code=404):
+            with self.assertRaises(HTTPError):
+                self.client.delete_organization('nada')
