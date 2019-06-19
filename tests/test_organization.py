05508b7e1e51d5062181242cad6036d6efee7ebb
Einar Forselv <eforselv@gmail.com>
2019-06-19 04:11:28 +0200

    Simplify tests

--- tests/test_organization.py
+++ tests/test_organization.py
@@ -1,21 +1,27 @@
-from unittest import TestCase, mock
-from base import PostalAdminTestcase
-from utils import fixture_response
+from requests.exceptions import HTTPError
 
-REQUEST_FUNC = 'requests.sessions.Session.request'
+from base import PostalAdminTestcase
+from utils import fixtures
 
 
 class OrganizationTests(PostalAdminTestcase):
 
     def test_list(self):
         """List organization"""
-        with mock.patch(REQUEST_FUNC, new=fixture_response()):
+        with fixtures():
             orgs = self.client.list_organizations()
             self.assertEqual(orgs, self.orgs)
 
     def test_create(self):
         """Create an organization"""
         json = {"redirect_to": "/org/il"}
-        with mock.patch(REQUEST_FUNC, new=fixture_response('POST', '/organizations', json=json)):
+        with fixtures('POST', '/organizations', json=json):
             org = self.client.create_organization('Illuminati')
             self.assertEqual(org, {'name': 'Illuminati', 'shortname': 'il'})
+
+    def test_create_existing(self):
+        """Esure 422 error is picked up when shortname is taken"""
+        json = {"error": "Shortname already taken"}
+        with fixtures('POST', '/organizations', json=json, status_code=422):
+            with self.assertRaises(HTTPError):
+                org = self.client.create_organization('Illuminati', shortname='il')
