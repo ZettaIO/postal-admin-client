9521d41673b4f6617a077a64484a18f249dee99d
Einar Forselv <eforselv@gmail.com>
2019-06-19 08:29:33 +0200

    More package configuration

--- setup.py
+++ setup.py
@@ -4,11 +4,21 @@ setup(
     name='postal-admin-client',
     version='0.1',
     url="https://github.com/ZettaIO/postal-admin-client",
+    description="Python admin client for the open source mail delivery platform Postal",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author="Einar Forselv",
     author_email="eforselv@gmail.com",
     packages=['postal_admin_client'],
     install_requires=[
         'requests>=2.22',
         'beautifulsoup4>=4.7',
+    ],
+    keywords = ['posta', 'admin', 'client'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
